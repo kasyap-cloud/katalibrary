ee56ecca023e94d524e64c2a860b72d04f9ddd82
kasyap-cloud <achusmarty1234@gmail.com>
2024-08-10 16:46:06 +0530

    class book created

---
+++ library.py
@@ -0,0 +1,7 @@
+class Book :
+    def __init__(self, isbn, title, author, year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year
+        
